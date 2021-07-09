d0ed343e0099805ebeae842ac5a01d5c5b35dd4e
Johnson <weihsun.chen@nogle.com>
2021-07-09 18:25:29 +0800

    adjust button style

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,9 +1,14 @@
 import style from './style.module.scss';
 
-export default function Button({ text = '', size = 'medium', onClick }) {
+export default function Button({
+  text = '', 
+  size = 'medium',
+  type = 'primary',
+  onClick
+}) {
   return (
     <button
-      className={`${style.container} ${style[size]}`}
+      className={`${style.container} ${style[size] || ''} ${type === 'simple' && style.simple}`}
       onClick={onClick}
     >
       {text}
