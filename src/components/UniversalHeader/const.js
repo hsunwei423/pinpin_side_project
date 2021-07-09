5c0bb9adc159b7ed6ddbe39427a7c7ba03d5d7a9
Johnson <weihsun.chen@nogle.com>
2021-07-09 17:44:32 +0800

    set basic link

---
+++ src/components/UniversalHeader/const.js
@@ -0,0 +1,22 @@
+export const NAV_LIST = [
+  {
+    title: 'Home',
+    key: 'home',
+    link: '/'
+  },
+  {
+    title: 'About',
+    key: 'about',
+    link: '/about'
+  },
+  {
+    title: 'Dig treasure',
+    key: 'Dig',
+    link: '/dig'
+  },
+  {
+    title: 'Idea',
+    key: 'idea',
+    link: '/idea'
+  }
+];
--- src/components/UniversalHeader/index.js
+++ src/components/UniversalHeader/index.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
+import { NAV_LIST } from './const';
+
 import style from './style.module.scss';
 
 export default function UniversalHeader() {
   return (
       <nav className={style.container}>
         <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
+          {
+            NAV_LIST.map((d, index) => {
+              return (
+                <li key={index}>
+                  <Link href={d.link}>
+                    <a>{d.title}</a>
+                  </Link>
+                </li>
+              )
+            })
+          }
         </ul>
       </nav>
   );
