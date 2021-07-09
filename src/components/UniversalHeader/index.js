da914c02c881d96ca1308a7df7efcf0f5783b199
Johnson <weihsun.chen@nogle.com>
2021-07-09 18:10:12 +0800

    adjust header with signin & signup button

--- src/components/UniversalHeader/index.js
+++ src/components/UniversalHeader/index.js
@@ -1,26 +1,37 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
+import Button from '@/components/common/Button';
+
 import { NAV_LIST } from './const';
 
 import style from './style.module.scss';
 
 export default function UniversalHeader() {
   return (
-      <nav className={style.container}>
-        <ul>
-          {
-            NAV_LIST.map((d, index) => {
-              return (
-                <li key={index}>
-                  <Link href={d.link}>
-                    <a>{d.title}</a>
-                  </Link>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </nav>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <nav>
+          <ul>
+            {
+              NAV_LIST.map((d, index) => {
+                return (
+                  <li key={index}>
+                    <Link href={d.link}>
+                      <a>{d.title}</a>
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+
+        <div className={style['log-area']}>
+          <Button text="Signin" size="small" />
+          <Button text="Signup" size="small" />
+        </div>
+      </div>
+    </div>
   );
 };
