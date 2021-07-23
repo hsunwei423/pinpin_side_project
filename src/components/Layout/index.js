636325aade6cc71dce3795a52b0e33fc3c4027fc
Johnson <weihsun.chen@nogle.com>
2021-07-23 09:53:24 +0800

    adjust layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 
 import UniversalHeader from '@/components/UniversalHeader';
 
+import style from './style.module.scss';
+
 export default function Layout({ children }) {
   return (
     <>
       <UniversalHeader />
-      <main>
+      <main className={style.main}>
         {children}
       </main>
       <footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,15 @@ import Head from 'next/head'
 import Layout from '@/components/Layout';
 import MainSale from '@/components/MainSale';
 
+import style from '@/styles/home.module.scss';
+
 export default function Home() {
   return (
     <Layout>
       <Head>
         <title>拼拼</title>
       </Head>
+
       <MainSale />
     </Layout>
   )
